969b7912c8df25a10e9c958833d65759da88e6b0
Donovan Pechetti <dovi_pf@hotmail.com>
2021-10-22 07:33:51 -0300

    login box created

---
+++ src/components/LoginBox/index.tsx
@@ -0,0 +1,20 @@
+import { useContext, useEffect } from 'react'
+import { VscGithubInverted } from 'react-icons/vsc'
+import { AuthContext } from '../../contexts/auth'
+import { api } from '../../services/api'
+
+import styles from './styles.module.scss'
+
+export function LoginBox() {
+  const { signInUrl } = useContext(AuthContext)
+
+  return (
+    <div className={styles.loginBoxWrapper}>
+      <strong>Entre e compartilhe sua mensagem</strong>
+      <a href={signInUrl} className={styles.signInWithGithub}>
+        <VscGithubInverted size='24' />
+        Entrar com o GitHub
+      </a>
+    </div>
+  )
+}
