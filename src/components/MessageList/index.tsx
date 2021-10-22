b4666d4c65069354c25d4906b7a5d17c6e72e35a
Donovan Pechetti <dovi_pf@hotmail.com>
2021-10-22 07:34:09 -0300

    message list created

---
+++ src/components/MessageList/index.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react'
+import { api } from '../../services/api'
+
+import styles from './styles.module.scss'
+import logo from '../../assets/logo.svg'
+
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  }
+}
+
+export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([])
+
+  useEffect(() => {
+    api.get<Message[]>('/messages/last3').then(response => setMessages(response.data))
+  }, [])
+
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logo} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        {messages.map(message => (
+          <li key={message.id} className={styles.messageItem}>
+          <p className={styles.messageContent}>{message.text}</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src={message.user.avatar_url} alt={message.user.name} />
+            </div>
+            <span>{message.user.name}</span>
+          </div>
+        </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
